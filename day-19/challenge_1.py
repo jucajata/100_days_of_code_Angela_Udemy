fe0b12e65286d2af026e1f4851937d2f929bdad0
jucajata <jcjt95@gmail.com>
2024-02-18 20:55:09 -0500

    fix: nombres de las carpetas e inicialización dia 19

---
+++ day-19/challenge_1.py
@@ -0,0 +1,28 @@
+'''Make an Etch-A-Sketch App'''
+import turtle as t
+
+tim = t.Turtle()
+screen = t.Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def move_counter_clockwise():
+    tim.setheading(tim.heading() + 10)
+
+def move_clockwise():
+    tim.setheading(tim.heading() - 10)
+
+def clear_drawing():
+    t.resetscreen()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counter_clockwise)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="c", fun=clear_drawing)
+screen.exitonclick()
